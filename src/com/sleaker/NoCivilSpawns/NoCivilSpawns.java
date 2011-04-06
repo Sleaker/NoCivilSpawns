aaa28f6d2020f7171fa7110914393bbc2680b45c
Nick Minkler <sleaker@gmail.com>
2011-04-06 14:32:08 -0700

    Renamed Project - originally Fork of LimitSpawn

---
+++ src/com/sleaker/NoCivilSpawns/NoCivilSpawns.java
@@ -0,0 +1,31 @@
+package com.sleaker.NoCivilSpawns;
+
+import java.util.logging.Logger;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoCivilSpawns extends JavaPlugin{
+	private final NoSpawnCreatureSpawnEvent spawnListener = new NoSpawnCreatureSpawnEvent(this);
+	
+	public static Logger log = Logger.getLogger("Minecraft");
+
+	public void onDisable() {
+		log.info("[NoCivilSpawns] Disabled");
+	}
+
+	public void onEnable() {
+        
+        //Create the pluginmanage pm.
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.CREATURE_SPAWN, spawnListener, Priority.Normal, this);
+        
+        //Get the infomation from the plugin.yml file.
+        PluginDescriptionFile pdfFile = this.getDescription();
+        //Print that the plugin has been enabled!
+        log.info("[LimitSpawns] version " + pdfFile.getVersion() + " by lonelydime & updated by Sleaker is enabled!");
+	}
+}
---
+++ src/com/sleaker/NoCivilSpawns/NoSpawnCreatureSpawnEvent.java
@@ -0,0 +1,79 @@
+package com.sleaker.NoCivilSpawns;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityListener;
+
+public class NoSpawnCreatureSpawnEvent extends EntityListener{
+	public static NoCivilSpawns plugin;
+	
+	public NoSpawnCreatureSpawnEvent(NoCivilSpawns instance) {
+        plugin = instance;
+    }
+	
+	public void onCreatureSpawn(CreatureSpawnEvent event) {
+		
+		int[] treeids = {17, 18};
+		int[] blacklistids = {4, 44, 45, 64, 67, 85};
+		int[] spawnerid = {52};
+		boolean spawner = false;
+		boolean allowSpawn = true;
+		//gets the block at the location of spawn
+		Block spawnblock = event.getEntity().getWorld().getBlockAt(event.getLocation().getBlockX(), event.getLocation().getBlockY()-1, event.getLocation().getBlockZ());
+		
+		
+
+		
+		if (allowSpawn) {
+			spawner = !testCuboid(1, 5, 5, spawnerid, spawnblock.getLocation());
+			//if (spawner)
+				//LimitSpawns.log.info("[LimitSpawns] - Spawner Detected - Allowing Spawn");
+		}
+		
+		//Test to make sure we aren't spawning on a tree or too close to one. for sure (wood blocks.)
+		if (allowSpawn && !spawner) {
+			allowSpawn = testCuboid(4, 5, -2, treeids, spawnblock.getLocation());
+			//if (!allowSpawn)
+				//LimitSpawns.log.info("[LimitSpawns] - Canceled Spawn - Attempted Tree Spawn");
+		}
+		if (allowSpawn && !spawner) {
+			allowSpawn = testCuboid(15, 20, 5, blacklistids, spawnblock.getLocation());
+			//if (!allowSpawn)
+				//LimitSpawns.log.info("[LimitSpawns] - Canceled Spawn - Too close to civilization");
+		}
+			
+		if (!allowSpawn && !spawner) {
+			event.setCancelled(true);
+		}
+	}
+
+	
+	public boolean testCuboid(int max, int sizeX, int sizeY, int[] blockids, Location blockloc) {
+		int count = 0;
+		boolean countUp = true;
+		if (sizeY < 0) {
+			countUp = false;
+			sizeY = -sizeY;
+		}
+		for (int i = 0; i < sizeY; i++)
+			for (int j = 0; j < sizeX; j++)
+				for (int k = 0; k < sizeX; k++) {
+					for (int l = 0; l < blockids.length; l++) {
+						
+						if (countUp) 
+							if ( blockids[l] == blockloc.getWorld().getBlockAt(blockloc.getBlockX() + j, blockloc.getBlockY() + i, blockloc.getBlockZ() + k).getTypeId() )
+							count++;
+						
+						if (!countUp)
+							if ( blockids[l] == blockloc.getWorld().getBlockAt(blockloc.getBlockX() + j, blockloc.getBlockY() - i, blockloc.getBlockZ() + k).getTypeId() )
+								count++;
+						
+					}
+					if (count >= max)
+						return false;
+				}
+		
+		return true;
+	}
+}
