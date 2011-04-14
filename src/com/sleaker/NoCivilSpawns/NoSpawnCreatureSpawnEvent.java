5a91b47ebbe93aff33affeb1f26e1018d1f0f49e
sleaker <sleaker@sleak-pc>
2011-04-14 01:22:57 -0700

    creature spawn options are now stored per-world

--- src/com/sleaker/NoCivilSpawns/NoSpawnCreatureSpawnEvent.java
+++ src/com/sleaker/NoCivilSpawns/NoSpawnCreatureSpawnEvent.java
@@ -7,6 +7,7 @@ package com.sleaker.NoCivilSpawns;
  *
  */
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,72 +17,65 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityListener;
 
 
+
 public class NoSpawnCreatureSpawnEvent extends EntityListener {
 	public final NoCivilSpawns plugin;
-	private static final Set<Integer> blockedIds = makeSet ( new int[] {4, 5, 20, 45} );
-	private static final Set<Integer> blacklistIds = makeSet( new int[] {4, 5, 20, 35, 43, 44, 45, 54, 62, 64, 65, 67, 85} );
-	private static final Set<Integer> treeIds = makeSet( new int[] {17, 18} );
-	private static final Set<Integer> spawnOkIds = makeSet( new int[] {52} );
 	private static final int goldId = 41;
 	private static final int ironId = 42;
+	private static final Set<Integer> treeIds = new HashSet<Integer>(Arrays.asList(new Integer[] {17, 18}) );
 
 
-	private static final Set<Integer> makeSet(final int[] array) {
-		Set<Integer> set = new HashSet<Integer>();
-		for (int i : array) {
-			set.add(i);
-		}
-		return set;
-	}
 	
 	public NoSpawnCreatureSpawnEvent(NoCivilSpawns instance) {
 		plugin = instance;
 	}
 	
+
 	public void onCreatureSpawn(CreatureSpawnEvent event) {
 
 		//gets the block at the location of spawn
 		Location spawnLocation = event.getLocation();
 		
-				
-		if (NoCivilSpawns.whitelistmobs.contains(event.getCreatureType().getName()) )
+		WorldSpawnConfiguration conf = NoCivilSpawns.worldConfig.get(event.getLocation().getWorld().getName());
+			
+		if (conf.getWhitelistMobs().contains(event.getCreatureType().getName()) )
 			return;
 		
-		if (NoCivilSpawns.blacklistmobs.contains(event.getCreatureType().getName()) ) {
+		if (conf.getBlacklistMobs().contains(event.getCreatureType().getName()) ) {
 			event.setCancelled(true);
 			return;
 		}
 				
 
 			
-		if ( NoCivilSpawns.diamondEnabler )
-			spawnOkIds.add(57);
+		if ( conf.getDiamondEnabler() )
+			conf.getSpawnOkIds().add(57);
 		
 		// Checks for a spawner in a 9x9x3 cuboid
-		if (testCuboid(1, 4, 0, 3, spawnOkIds, spawnLocation)) {
+		if (testCuboid(1, 4, 0, 3, conf.getSpawnOkIds(), spawnLocation, conf)) {
 			//NoCivilSpawns.log.info(NoCivilSpawns.plugName + " - Spawner Detected - Allowing Spawn");
 			return;
 		}
 
-		if (NoCivilSpawns.goldBlocker)
-			blockedIds.add(goldId);
+		if ( conf.getGoldBlocker() )
+			conf.getBlockedIds().add(goldId);
 
 		
 		// Check to see if we are spawning directly on one of these blocks, if we are, then abort.
-		if (NoCivilSpawns.quick)
-			if (testCuboid(1, 1, -1, -1, blockedIds, spawnLocation)) {
+		if ( conf.getQuick())
+			if (testCuboid(1, 1, -1, -1, conf.getBlockedIds(), spawnLocation, conf)) {
 				event.setCancelled(true);
 				return;
 			}
 		
 		//Test to make sure we aren't spawning on a tree or too close to one. for sure (wood blocks.)
-		if (testCuboid(4, 2, -2, 0, treeIds, spawnLocation)) {
+		if (testCuboid(4, 2, -2, 0, treeIds, spawnLocation, conf)) {
 			//NoCivilSpawns.log.info(NoCivilSpawns.plugName + " - Canceled Spawn - Attempted Tree Spawn");
 			event.setCancelled(true);
 			return;
 		}
 
-		if (testCuboid(15, 10, -2, 3, blacklistIds, spawnLocation)) {
+		if (testCuboid(15, 10, -2, 3, WorldSpawnConfiguration.getBlacklistids(), spawnLocation, conf)) {
 			//NoCivilSpawns.log.info(NoCivilSpawns.plugName + " - Canceled Spawn - Too close to civilization");
 			event.setCancelled(true);
 			return;
@@ -89,7 +83,7 @@ public class NoSpawnCreatureSpawnEvent extends EntityListener {
 	}
 
 
-	public static final boolean testCuboid(int max, int radius, int minY, int maxY, Set<Integer> blockIds, Location blockloc) {
+	public static final boolean testCuboid(int max, int radius, int minY, int maxY, Set<Integer> blockIds, Location blockloc, WorldSpawnConfiguration conf) {
 		final World world = blockloc.getWorld();
 		final int blockX = blockloc.getBlockX();
 		final int blockY = blockloc.getBlockY();
@@ -105,9 +99,9 @@ public class NoSpawnCreatureSpawnEvent extends EntityListener {
 						if (count >= max)
 							return true;
 					}
-					if (NoCivilSpawns.goldBlocker && blockId == goldId)
+					if ( conf.getGoldBlocker() && blockId == goldId)
 						return true;
-					if (NoCivilSpawns.ironBlocker && blockId == ironId )
+					if ( conf.getIronBlocker() && blockId == ironId )
 						return true;
 				}
 			}
