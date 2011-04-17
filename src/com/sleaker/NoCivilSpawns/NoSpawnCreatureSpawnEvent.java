13ab95dc30539499372074ceac3b1f1452fcde46
sleaker <sleaker@sleak-pc>
2011-04-16 22:48:27 -0700

    fixed detection with true/false options

--- src/com/sleaker/NoCivilSpawns/NoSpawnCreatureSpawnEvent.java
+++ src/com/sleaker/NoCivilSpawns/NoSpawnCreatureSpawnEvent.java
@@ -20,37 +20,36 @@ import org.bukkit.event.entity.EntityListener;
 
 public class NoSpawnCreatureSpawnEvent extends EntityListener {
 	public final NoCivilSpawns plugin;
+	private static final Set<Integer> blockerIds = new HashSet<Integer>(Arrays.asList(new Integer[] {41, 42}));
+	private static final Set<Integer> treeIds = new HashSet<Integer>(Arrays.asList(new Integer[] {17, 18}) );
 	private static final int goldId = 41;
 	private static final int ironId = 42;
-	private static final Set<Integer> treeIds = new HashSet<Integer>(Arrays.asList(new Integer[] {17, 18}) );
 
 
-	
+
 	public NoSpawnCreatureSpawnEvent(NoCivilSpawns instance) {
 		plugin = instance;
 	}
-	
+
 
 	public void onCreatureSpawn(CreatureSpawnEvent event) {
 
 		//gets the block at the location of spawn
 		Location spawnLocation = event.getLocation();
-		
+
 		WorldSpawnConfiguration conf = NoCivilSpawns.worldConfig.get(event.getLocation().getWorld().getName());
-			
+
 		if (conf.getWhitelistMobs().contains(event.getCreatureType().getName()) )
 			return;
-		
+
 		if (conf.getBlacklistMobs().contains(event.getCreatureType().getName()) ) {
 			event.setCancelled(true);
 			return;
 		}
-				
 
-			
 		if ( conf.getDiamondEnabler() )
 			conf.getSpawnOkIds().add(57);
-		
+
 		// Checks for a spawner in a 9x9x3 cuboid
 		if (testCuboid(1, 4, 0, 3, conf.getSpawnOkIds(), spawnLocation, conf)) {
 			//NoCivilSpawns.log.info(NoCivilSpawns.plugName + " - Spawner Detected - Allowing Spawn");
@@ -60,14 +59,17 @@ public class NoSpawnCreatureSpawnEvent extends EntityListener {
 		if ( conf.getGoldBlocker() )
 			conf.getBlockedIds().add(goldId);
 
-		
+		if (conf.getIronBlocker() )
+			conf.getBlockedIds().add(ironId);
+
+
 		// Check to see if we are spawning directly on one of these blocks, if we are, then abort.
-		if ( conf.getQuick())
+		if ( conf.getQuickTest())
 			if (testCuboid(1, 1, -1, -1, conf.getBlockedIds(), spawnLocation, conf)) {
 				event.setCancelled(true);
 				return;
 			}
-		
+
 		//Test to make sure we aren't spawning on a tree or too close to one. for sure (wood blocks.)
 		if (testCuboid(4, 2, -2, 0, treeIds, spawnLocation, conf)) {
 			//NoCivilSpawns.log.info(NoCivilSpawns.plugName + " - Canceled Spawn - Attempted Tree Spawn");
@@ -98,10 +100,8 @@ public class NoSpawnCreatureSpawnEvent extends EntityListener {
 						count++;
 						if (count >= max)
 							return true;
-					}
-					if ( conf.getGoldBlocker() && blockId == goldId)
-						return true;
-					if ( conf.getIronBlocker() && blockId == ironId )
+					}					
+					if ( (conf.getGoldBlocker() || conf.getIronBlocker() ) && blockerIds.contains(blockId) ) 
 						return true;
 				}
 			}
